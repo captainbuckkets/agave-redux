2e1ff71ff7f0f83599fa095261aa53c1167708fd
j <jack.day@protonmail.com>
2020-09-17 07:41:33 -0600

    Login page mockup done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
+// Login screen loaders
+// Add event listeners to Sign In and New User tabs
+function prepFunction() {
+    var login = document.getElementById("loginTab")
+    var newUsr = document.getElementById("newUserTab")
+
+    // Add event listeners for swapping between login tabs
+    login.addEventListener("click", openLogin)
+    newUsr.addEventListener("click", openNewUser)
+}
+
+// Show the loading screen
+function loaderStart() {
+    // Turn on the loader
+    document.getElementById("loader").style.display = "flex"
+    setTimeout(loaderStop, 300)
+}
+
+function loaderStop() {
+    // Turn off the loader
+    document.getElementById("loader").style.display = "none" 
+}
+
+// Tab Swapping functions for login screen
+function openLogin() {
+    document.getElementById("newUserDiv").style.display = "none"
+    document.getElementById("returningUserDiv").style.display = "block"
+}
+
+function openNewUser() {
+    document.getElementById("returningUserDiv").style.display = "none"
+    document.getElementById("newUserDiv").style.display = "block"
+}
 
 // Check if the login credentials are accepted and load the required information.  For now this is empty
 function loginCheck() {
@@ -9,6 +42,7 @@ function loadWalletArea() {
     // Hide the login area - maybe add an animation
     document.getElementById("loginArea").style.display = "none"
     // Show the wallet area
+    document.getElementById("navBar").style.display = "inline-flex"
     document.getElementById("walletDiv").style.display = "inline-flex"
     document.getElementById("overviewDiv").style.display = "grid"
     // Show our nav
@@ -39,4 +73,11 @@ function loadSend() {
 
     // Show Overview
     document.getElementById("sendDiv").style.display = "grid"
+}
+
+// Logout function
+function logout() {
+    // Alert to make sure you want to log out
+    // Remove creds
+    // Remove elements
 }
